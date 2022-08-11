a672b54eb8a787edc4cca4256b4f03476d75461b
Ward McKinley <wmck126@gmail.com>
2022-08-10 21:00:46 -0400

    met basic requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
-Trainer.create(name: "Ash", image_url:"https://static.wikia.nocookie.net/nintendo/images/0/0f/Ash_Ketchum.png/revision/latest?cb=20111019135611&path-prefix=en", gym_id: nil )
-Trainer.create(name: "Brock", image_url:"https://static.wikia.nocookie.net/nintendo/images/d/dd/Brock_Lets_Go_Pikachu_Eevee.png/revision/latest/scale-to-width-down/202?cb=20220201052059&path-prefix=en", gym_id: 1)
-Trainer.create(name: "Misty", image_url:"https://static.wikia.nocookie.net/nintendo/images/7/73/Misty_%28Anime_-_1%29.png/revision/latest/scale-to-width-down/250?cb=20121111060531&path-prefix=en", gym_id: 2)
-Trainer.create(name: "Lt Surge", image_url:"https://static.wikia.nocookie.net/nintendo/images/5/5f/LGPELTSurge.png/revision/latest/scale-to-width-down/350?cb=20220124111302&path-prefix=en", gym_id: 3)
-Trainer.create(name: "Erika", image_url:"https://static.wikia.nocookie.net/nintendo/images/a/a7/ErikaLetsGo.png/revision/latest/scale-to-width-down/203?cb=20220124002544&path-prefix=en", gym_id: 4)
+Trainer.create(name: "Ash", image_url:"http://improveyourdrawings.com/wp-content/uploads/2019/02/Step-12-Shadows.jpg", gym_id: nil )
+Trainer.create(name: "Brock", image_url:"https://upload.wikimedia.org/wikipedia/en/7/71/DP-Brock.png", gym_id: 1)
+Trainer.create(name: "Misty", image_url:"https://upload.wikimedia.org/wikipedia/en/b/b1/MistyEP.png", gym_id: 2)
+Trainer.create(name: "Lt Surge", image_url:"https://static.wikia.nocookie.net/pokemon/images/9/95/Lt._Surge_HeartGold_and_SoulSilver.png", gym_id: 3)
+Trainer.create(name: "Erika", image_url:"https://i.pinimg.com/236x/85/75/42/857542660e64c7d67c6d226e824687c0.jpg", gym_id: 4)
 
 Gym.create(gym: "Rock", location: "Pewter City")
 Gym.create(gym: "Water", location: "Cerulean City")
